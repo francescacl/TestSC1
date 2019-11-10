81c918e615c6888201eaee05448e40af49c6fe78
francescacl <45533212+francescacl@users.noreply.github.com>
2019-11-10 16:47:38 +0100

    Done

--- src/prova/Main.java
+++ src/prova/Main.java
@@ -1,11 +1,12 @@
 package prova;
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
 
 public class Main {
 
-	static java.lang.System.Logger logger = Logger.getLogger(Main.class.getName());
+	//static java.lang.System.Logger logger = Logger.getLogger(Main.class.getName());
 	public static void main(String[] args) {
-		logger.log(Level.info, "Hello World.");
+		//logger.log(Level.info, "Hello World.");
+		System.out("Hello World");
 	}
 	
 }
