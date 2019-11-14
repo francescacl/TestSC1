5106b16a10d768d0dd24c980aa2f80fcb3bda8fc
francescacl <45533212+francescacl@users.noreply.github.com>
2019-11-14 17:00:36 +0100

    Done

---
+++ src/prova/Hello.java
@@ -0,0 +1,12 @@
+package prova;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Hello {
+
+	static Logger logger = Logger.getLogger(Hello.class.getName());
+	public static void main(String[] args) {
+		logger.log(Level.INFO, "Hello World.");
+	}
+	
+}
